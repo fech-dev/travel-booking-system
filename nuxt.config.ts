2244fc90556ea79f9e8f2cbf07a89960e4594c56
Federico Mameli <federico_mameli@yahoo.com>
2024-01-19 20:42:34 +0000

    feat: install tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/test-utils/module'
+    '@nuxt/test-utils/module',
+    '@nuxtjs/tailwindcss'
   ],
 
   runtimeConfig: {

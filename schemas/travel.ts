c9cc0e19eab68142ad08f48df3cfbc854e93e2ea
Federico Mameli <federico_mameli@yahoo.com>
2024-01-19 00:41:20 +0000

    feat(schemas): create Travel schemas

---
+++ schemas/travel.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+
+export const TravelSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  picture: z.string(),
+  price: z.string(),
+  average_user_rating: z.number(),
+  starts_at: z.string(),
+  ends_at: z.string()
+})
+
+export type Travel = z.infer<typeof TravelSchema>
+
+export const CreateTravelBodySchema = TravelSchema.omit({ id: true })
+export type CreateTravelBody = z.infer<typeof CreateTravelBodySchema>
+
+export const EditTravelBodySchema = CreateTravelBodySchema.partial()
+export type EditTravelBody = z.infer<typeof EditTravelBodySchema>

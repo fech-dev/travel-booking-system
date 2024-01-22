90cad8c588dc6c67c57b8c31bd71c4ad65b2318e
Federico Mameli <federico_mameli@yahoo.com>
2024-01-22 23:59:05 +0000

    feat: change font family

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
           crossorigin: "",
         },
         {
-          href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap",
+          href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap",
           rel: "stylesheet",
         },
       ],

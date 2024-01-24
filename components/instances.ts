6fa9c2adccc096373f51afc3526ce688b90f83c5
Federico Mameli <federico_mameli@yahoo.com>
2024-01-24 20:40:28 +0000

    feat(components): add component instances types

---
+++ components/instances.ts
@@ -0,0 +1,11 @@
+export type ModalInstance = InstanceType<
+  (typeof import("~/components/Ui/Modal.vue"))["default"]
+>;
+
+export type DataTableInstance = InstanceType<
+  (typeof import("~/components/Ui/DataTable.vue"))["default"]
+>;
+
+export type TravelsTableInstance = InstanceType<
+  (typeof import("~/components/TravelsTable.vue"))["default"]
+>;

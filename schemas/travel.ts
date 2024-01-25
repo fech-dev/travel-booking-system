4bd7c1330c95a8cee8b2526a9ee197df073ce9cb
Federico Mameli <federico_mameli@yahoo.com>
2024-01-25 12:30:06 +0000

    style: remove comments

--- schemas/travel.ts
+++ schemas/travel.ts
@@ -27,20 +27,13 @@ export type Travel = z.infer<typeof TravelSchema>;
 
 export const CreateTravelBodySchema = TravelSchema.omit({
   id: true,
-  // departure_date: true,
-  // return_date: true,
-})
-  // .extend({
-  //   departure_date: z.coerce.date().or(z.string()),
-  //   return_date: z.coerce.date().or(z.string()),
-  // })
-  .refine(
-    (schema) => greaterThanDate(schema.return_date, schema.departure_date),
-    {
-      path: ["return_date"],
-      message: "Return date must be greater than departure date",
-    }
-  );
+}).refine(
+  (schema) => greaterThanDate(schema.return_date, schema.departure_date),
+  {
+    path: ["return_date"],
+    message: "Return date must be greater than departure date",
+  }
+);
 export type CreateTravelBody = z.infer<typeof CreateTravelBodySchema>;
 
 export const EditTravelBodySchema = z.optional(CreateTravelBodySchema);

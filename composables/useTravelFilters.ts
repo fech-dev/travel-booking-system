d147be4fdd9a35490ca879f22b9c5a5b19606dc3
Federico Mameli <federico_mameli@yahoo.com>
2024-01-25 17:54:03 +0000

    feat(travels): add filter by rating

--- composables/useTravelFilters.ts
+++ composables/useTravelFilters.ts
@@ -1,22 +1,47 @@
 import type { Travel } from "~/schemas";
 
 interface TravelFilters {
-  name: string;
+  name?: string;
+  rating?: number;
+}
+
+function filterByName(data: Travel[], value: string) {
+  return data.filter(({ name }) => name.includes(value));
+}
+
+function filterByRating(data: Travel[], value: number) {
+  return data.filter((travel) => {
+    return value === Math.floor(travel.average_user_rating);
+  });
 }
 
 export const useTravelFilters = (
   data: Ref<Travel[] | null>,
   filters: Ref<TravelFilters>
 ) => {
+  const options = {
+    rating: [
+      { label: "⭐", value: 1 },
+      { label: "⭐⭐", value: 2 },
+      { label: "⭐⭐⭐", value: 3 },
+      { label: "⭐⭐⭐⭐", value: 4 },
+      { label: "⭐⭐⭐⭐⭐", value: 5 },
+    ],
+  };
+
   const filteredData = computed(() => {
     let _data = [...(data?.value || [])];
 
-    if (filters.value.name) {
-      _data = _data.filter(({ name }) => name.includes(filters.value.name));
+    if (filters.value.name !== undefined) {
+      _data = filterByName(_data, filters.value.name!);
+    }
+
+    if (filters.value.rating) {
+      _data = filterByRating(_data, filters.value.rating);
     }
 
     return _data;
   });
 
-  return { filteredData };
+  return { filteredData, options };
 };
